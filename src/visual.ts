f92a6bdff4767f76c3eed0a470a93a1cbfb93630
Mike Chan <chanm003@gmail.com>
2021-08-16 12:33:32 +0000

    render map on load

--- src/visual.ts
+++ src/visual.ts
@@ -1,6 +1,7 @@
 "use strict";
 
 import "core-js/stable";
+import "leaflet/dist/leaflet.css";
 import "./../style/visual.less";
 import powerbi from "powerbi-visuals-api";
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
@@ -10,21 +11,48 @@ import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInst
 import VisualObjectInstance = powerbi.VisualObjectInstance;
 import DataView = powerbi.DataView;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
-
+import * as L from 'leaflet';
 import * as tileLayers from "../tilelayers.json";
 import { VisualSettings } from "./settings";
 export class Visual implements IVisual {
     private target: HTMLElement;
     private settings: VisualSettings;
+    private mapContainer: HTMLElement;
+    private map: L.Map;
 
     constructor(options: VisualConstructorOptions) {
+        if (!document) { return; }
         this.target = options.element;
-        const layers = this.getTileLayers();
-        console.log(layers);
+        this.createMapContainer();
+        this.configureLeaflet();
     }
 
     public update(options: VisualUpdateOptions) {
         this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+        this.resizeMap(options);
+    }
+
+    public destroy() {
+        this.map.remove();
+    }
+
+    private configureLeaflet() {
+        // create L.Map off of the <div>
+        this.map = new L.Map("mapid");
+        this.map.setView(new L.LatLng(48.78, 9.18), 13);
+        const layers = this.getTileLayers();
+        layers.forEach(url => this.map.addLayer(L.tileLayer(url)));
+        this.map.attributionControl.setPrefix(false);
+    }
+
+    private createMapContainer() {
+        // add <div> to the DOM
+        const div = document.createElement('div');
+        div.id = 'mapid';
+        div.style.width = `${this.target.clientWidth}px`;
+        div.style.height = `${this.target.clientHeight}px`;
+        this.mapContainer = div;
+        this.target.append(div);
     }
 
     private getTileLayers(): string[] {
@@ -39,6 +67,13 @@ export class Visual implements IVisual {
 
     private static parseSettings(dataView: DataView): VisualSettings {
         return <VisualSettings>VisualSettings.parse(dataView);
+    }
+
+    private resizeMap(options: VisualUpdateOptions) {
+        const { width, height } = options.viewport;
+        this.mapContainer.style.width = width + 'px';
+        this.mapContainer.style.height = height + 'px';
+        this.map.invalidateSize(true);
     }
 
     /**
